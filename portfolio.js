be49c6ab80143d3c6829e3606b767cff6ba2920f
vivixnc <52637721+vivixnc@users.noreply.github.com>
2019-07-26 11:20:10 -0400

    Create portfolio.js

---
+++ portfolio.js
@@ -0,0 +1,8 @@
+function showMeme()
+{
+  var x = document.createElement("IMG");
+  x.setAttribute("src", "https://i.pinimg.com/originals/fb/38/4a/fb384ac26c99b7393708a86c235ddd2a.jpg");
+  x.setAttribute("width", "304");
+  x.setAttribute("height", "228");
+  document.body.appendChild(x);
+}
