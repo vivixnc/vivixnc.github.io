af31809048dcc6b095a66927e3e4fbda36add3a2
vivixnc <52637721+vivixnc@users.noreply.github.com>
2019-07-26 11:39:43 -0400

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -2,7 +2,7 @@ function showMeme()
 {
   var x = document.createElement("IMG");
   x.setAttribute("src", "https://i.pinimg.com/originals/fb/38/4a/fb384ac26c99b7393708a86c235ddd2a.jpg");
-  x.setAttribute("width", "500");
-  x.setAttribute("height", "500");
+  x.setAttribute("width", "1000");
+  x.setAttribute("height", "1000");
   document.body.appendChild(x);
 }
